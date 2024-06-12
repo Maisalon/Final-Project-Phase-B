69d55c664acbbb1a4e37a214dfd4fb73e0f7d309
Maisalon Safory <101040454+Maisalon@users.noreply.github.com>
2024-06-12 18:38:28 +0300

    server

---
+++ GameServer/Server/Controllers/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedLibrary;
+namespace Server.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class PlayerController : ControllerBase
+{
+    [HttpGet]
+
+    public Player Get([FromQuery] int id)
+    {
+        var player = new Player() { Id = id };
+        return player;
+    }
+    [HttpPost]
+    public Player Post(Player player)
+    {
+        Console.WriteLine("Player hase been added to the database");
+        return player;
+    }
+}
