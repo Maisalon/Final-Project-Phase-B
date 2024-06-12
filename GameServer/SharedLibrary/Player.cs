8f075a552a7f57d0984076ab93ff62e269a399a1
Maisalon Safory <101040454+Maisalon@users.noreply.github.com>
2024-06-12 20:51:06 +0300

    nm

--- GameServer/SharedLibrary/Player.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SharedLibrary
-{
-    public class Player
-    {
-        public int Level {  get; set; }
-        public int Id { get; set; }
-        public float Health { get; set; }
-    }
-}
